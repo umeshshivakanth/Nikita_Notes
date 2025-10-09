d84b93b28377b67eb18591ec8cf987028f7e5907
Umesh Shivakanth <Umesh.Shivakanth@cscglobal.com>
2025-10-09 16:43:22 +0530

    Code changes

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApplication.Data;
+using NotesApplication.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NotesApplication.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public  IActionResult Index()
+        {
+            Console.WriteLine($"context is {_context == null}");
+          var notes = _context.Notes.OrderByDescending(n=>n.CreatedAt).ToList();
+            return View(notes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+
+        [HttpPost]
+        public IActionResult Create(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Notes.Add(note);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(note);
+        }
+
+        public IActionResult Edit(int id )
+        {
+            var note = _context.Notes.Find(id);
+            if ((note == null))
+                return NotFound();
+            return View(note);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(int id, Note note)
+        {
+
+            if(id!=note.Id) return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Update(note);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(note);
+        }
+
+
+
+      
+        public IActionResult Delete(int id)
+        {
+
+            var note = _context.Notes.Find(id);
+            if ((note == null))
+                return NotFound();
+            return View(note);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+
+            var note = _context.Notes.Find(id);
+            if ((note != null))
+            {
+                _context.Notes.Remove(note);
+                _context.SaveChanges();
+
+
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotesApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<NotesApplication.Models.Note> Notes { get; set; }
+
+    }
+}
